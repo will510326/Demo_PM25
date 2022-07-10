f2a405b716bdc87af9239168161fa3d6faef31f8
Wayne <Wayne@gmail.com>
2022-07-10 11:36:00 +0800

    debug 403 error

--- static/js/pm25-charts.js
+++ static/js/pm25-charts.js
@@ -1,23 +1,26 @@
-var chartDom = document.getElementById('main');
-var myChart = echarts.init(chartDom);
-var option;
+const chart1E1 = document.querySelector('#main');
+let chart1 = echarts.init(chart1E1);
 
-option = {
-    xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [{
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar',
-        showBackground: true,
-        backgroundStyle: {
-            color: 'rgba(180, 180, 180, 0.2)'
-        }
-    }]
-};
+function drawPM25() {
+    var option;
 
-option && myChart.setOption(option);
+    option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+                color: 'rgba(180, 180, 180, 0.2)'
+            }
+        }]
+    };
+
+    option && chart1.setOption(option);
+}
